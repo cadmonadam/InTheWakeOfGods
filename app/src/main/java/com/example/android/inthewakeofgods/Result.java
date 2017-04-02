4fb8d51704d97758205988fed05ed23ceb8e5145
Gergő Siska <cadmonadam@gmail.com>
2017-04-02 15:29:34 +0200

    workflow2

---
+++ app/src/main/java/com/example/android/inthewakeofgods/Result.java
@@ -0,0 +1,17 @@
+package com.example.android.inthewakeofgods;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Adam Cadmon on 2017. 04. 02..
+ */
+
+public class Result extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.result);
+    }
+}
